1d37fbe7685fcc6ef738532d8f79693c7824fd62
Ivan Kondzerau <konderev.ivan@gmail.com>
2020-02-02 14:17:05 +0100

    fix: message

--- js/main.js
+++ js/main.js
@@ -1,8 +1,43 @@
 'use strict';
 
+var NAMES_ARR = [
+  'Валера',
+  'Толик',
+  'Галя',
+  'Зина',
+  'Иннокентий'
+];
+
+var MESSAGE_ARR = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+var COMMENTS_ARR = [];
+
 var randomRange = function (min, max) {
   return Math.floor(Math.random() * max) + min;
 };
+
+var genMessage = function () {
+  var MESSAGE = '';
+  for (var o = 1; o <= randomRange(1, 2); o++) {
+    MESSAGE += MESSAGE_ARR[randomRange(0, 5)] + ',';
+  }
+  return MESSAGE;
+};
+// Fill COMMENTS_ARR
+for (var u = 0; u < 25; u++) {
+  COMMENTS_ARR.push({
+    avatar: 'img/avatar-' + randomRange(1, 6) + '.svg',
+    message: genMessage(),
+    name: NAMES_ARR[randomRange(0, 4)]
+  });
+}
 
 var generatePicDescription = function () {
   var arrPicDesc = [];
@@ -11,24 +46,7 @@ var generatePicDescription = function () {
       url: 'photos/' + i + '.jpg',
       description: '',
       likes: randomRange(15, 200),
-      comments: [].push({
-        avatar: 'img/avatar-' + randomRange(1, 6) + '.svg',
-        message: [
-          'Всё отлично!',
-          'В целом всё неплохо. Но не всё.',
-          'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-          'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-          'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-          'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-        ][randomRange(0, 5)],
-        name: [
-          'Валера',
-          'Толик',
-          'Галя',
-          'Зина',
-          'Иннокентий'
-        ][randomRange(0, 4)]
-      })
+      comments: COMMENTS_ARR[randomRange(0, 24)]
     });
   }
   return arrPicDesc;
